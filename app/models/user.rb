022e357f87a15139c459f58f930e17e33e4ab27c
vic778 <victoiremmanuelbarh@gmail.com>
2022-11-18 19:35:18 +0400

    Generate the token for ech user

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,14 +2,15 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+         :recoverable, :rememberable, :validatable, :confirmable
 
   belongs_to :role
+  validates :name, presence: true
 
   after_initialize :set_default_role
 
   def admin?
-    self.role.name == 'admin'
+    role.name == 'admin'
   end
 
   def has_permission?(action, resource)
@@ -22,11 +23,20 @@ class User < ApplicationRecord
 
   def update_role!(role_name)
     self.role = Role.find_or_create_by(name: role_name)
-    self.save!
+    save!
+  end
+
+  def requires_confirmation?
+    !confirmed?
+  end
+
+  def generate_jwt
+    JWT.encode({ id:, exp: 30.days.from_now.to_i }, 'vicSecret')
   end
 
   private
+
   def set_default_role
-    self.role = Role.find_or_create_by(name: 'user') if self.role.nil?
+    self.role = Role.find_or_create_by(name: 'student') if role.nil?
   end
 end
