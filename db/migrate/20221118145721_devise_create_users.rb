de65b721e81452977cd74ec16fb04e9a2bd391bd
vic778 <victoiremmanuelbarh@gmail.com>
2022-11-18 19:00:36 +0400

    G migration for add a name to user and associate user to the role

--- db/migrate/20221118145721_devise_create_users.rb
+++ db/migrate/20221118145721_devise_create_users.rb
@@ -32,6 +32,7 @@ class DeviseCreateUsers < ActiveRecord::Migration[7.0]
       # t.string   :unlock_token # Only if unlock strategy is :email or :both
       # t.datetime :locked_at
 
+      t.references :role, null: false, foreign_key: true
 
       t.timestamps null: false
     end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_11_18_145437) do
+ActiveRecord::Schema[7.0].define(version: 2022_11_18_145932) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -31,11 +31,15 @@ ActiveRecord::Schema[7.0].define(version: 2022_11_18_145437) do
     t.datetime "confirmed_at"
     t.datetime "confirmation_sent_at"
     t.string "unconfirmed_email"
+    t.bigint "role_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "name"
     t.index ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+    t.index ["role_id"], name: "index_users_on_role_id"
   end
 
+  add_foreign_key "users", "roles"
 end
