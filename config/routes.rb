31679ccfe18320cb9484e3246035e96c22f1618d
vic778 <victoiremmanuelbarh@gmail.com>
2022-11-18 18:55:21 +0400

    Generate user model

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  devise_for :users
   mount Rswag::Ui::Engine => '/api-docs'
   mount Rswag::Api::Engine => '/api-docs'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
