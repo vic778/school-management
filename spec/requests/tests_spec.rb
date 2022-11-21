399f9ef311b0357090d8e4d96ff2c2bcccd7ca38
vic778 <victoiremmanuelbarh@gmail.com>
2022-11-21 20:11:06 +0400

    Create header to get the toekn

--- spec/requests/tests_spec.rb
+++ spec/requests/tests_spec.rb
@@ -1,7 +1,25 @@
 require 'rails_helper'
+require_relative '../features/sign_in_rspec'
 
 RSpec.describe "Tests", type: :request do
-  describe "GET /index" do
-    pending "add some examples (or delete) #{__FILE__}"
+  context "when as a teacher" do
+    before(:each) do
+      @user = sign_in_teacher
+      # sign_in @user
+      @headers = {
+        'Content-Type': 'application/json',
+        Accept: 'application/json',
+        Authorization: "Bearer #{@user['token']}",
+        uid: @user['email']
+      }
+    end
+
+    describe "Create a test" do
+      it "creates a test" do
+        # binding.pry
+        post '/api/tests', params: { test: { name: 'Test 136633', description: 'MyText' } }.to_json, headers: @headers
+        expect(response).to have_http_status(:created)
+      end
+    end
   end
 end
