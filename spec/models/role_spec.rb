4310e649cb628a86d99a84b0671c25e2b00f0ce0
vic778 <victoiremmanuelbarh@gmail.com>
2022-11-21 10:50:03 +0400

    Describe validations of values

--- spec/models/role_spec.rb
+++ spec/models/role_spec.rb
@@ -1,5 +1,10 @@
 require 'rails_helper'
 
 RSpec.describe Role, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it 'has a valid factory' do
+    expect(FactoryBot.build(:role)).to be_valid
+  end
+
+  it { is_expected.to have_many(:users) }
+  it { is_expected.to validate_presence_of(:name) }
 end
